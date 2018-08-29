d6427bef764d2b1080033852b600fb1762fe98c0
Michael Gatewood <michael@mngatewood.com>
2018-08-29 13:45:39 -0600

    Adds iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class House
 
   attr_reader :price, :address, :rooms
@@ -10,6 +12,20 @@ class House
 
   def add_room(room)
     @rooms << room
+  end
+
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      room.category == category
+    end
+  end
+
+  def area
+    room_areas = @rooms.map do |room|
+      room.area
+    end
+    room_areas.sum
+    binding.pry
   end
 
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -15,13 +15,41 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
     room_1 = Room.new(:bedroom, 10, 13)
     room_2 = Room.new(:bedroom, 11, 15)
-
     house.add_room(room_1)
     house.add_room(room_2)
 
     assert_equal 2, house.rooms.count
     assert_equal 130, house.rooms[0].area
     assert_equal 165, house.rooms[1].area
+  end
+
+  def test_it_can_have_more_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    
+    assert_equal 2, house.rooms_from_category(:bedroom).count
+    assert_equal 1, house.rooms_from_category(:basement).count
+  end
+
+  def test_it_has_house_area
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 1900, house.area
   end
 
 end
